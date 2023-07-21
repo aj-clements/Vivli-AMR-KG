d41408eb43465c6a2425d53c22da873568050d79
NaomiWaterlow <naomi.waterlow1@lshtm.ac.uk>
2023-07-21 16:59:28 +0100

    flexible choice of bug-drug

--- regression_analysis.R
+++ regression_analysis.R
@@ -3,26 +3,35 @@ library(data.table)
 library(MASS)
 
 full_data <- as.data.table(read.csv("data/full_data.csv"))
+
+# specify which bacteria and antibiotic of interest
+# NOTE - must match spelling in the data table
+target_antibiotic <- "levofloxacin"
+target_bug <- 'Staphylococcus aureus'
+
 # subset to just look at one bug-dryg
-lev_staph <- full_data[antibiotic == "levofloxacin" & 
-                         organism_clean == 'Staphylococcus aureus']
+sub_data <- full_data[antibiotic == target_antibiotic & 
+                         organism_clean == target_bug]
+
+# make children the base age group 
+sub_data$age_group <- factor(sub_data$age_group, levels = c(
+  "3 to 12 Years", 
+  "0 to 2 Years",
+   "13 to 18 Years",
+  "19 to 64 Years", 
+   "65 to 84 Years", 
+   "85 and Over"
+  
+))
 
 # convert to categorical for eah value
-lev_staph[, mic_cat_all := factor(11 + round(log(mic)/log(2)))]
+sub_data[, mic_cat_all := round(log(mic)/log(2))]
+add_to_make_0 <- -min(unique(sub_data$mic_cat_all))
+sub_data[, mic_cat_all := factor(add_to_make_0 + round(log(mic)/log(2)))]
 
 # try running a proportional odds ordinal model
-ord_mod <- polr(mic_cat_all ~ age_group + gender  + key_source , data = lev_staph)
+ord_mod <- polr(mic_cat_all ~ age_group + gender  + key_source + year, data = sub_data)
 summary(ord_mod)
-#odds of the variables
-exp(coef(ord_mod))
-#percentage change
-(exp(coef(ord_mod))-1)*100
-# older = more likely to have higher MIC. Gender and source make a difference but much less
-#but also varies a lot by source!
 
-
-##### QUESTIONS
-# Do want to also take account of year? 
-# and countries? This would probably require a multi-level model!
-# Or could do region which could probably be this or multilevel?
-# proportional odds the right model? I.e. is the assumption of proportionality appropiate?
+model_parameters <- summary(ord_mod)[1]$coefficients
+print(model_parameters)
