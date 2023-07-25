8e2c95d57911fd7e0fc9395ffc8dc2c71dc936be
gwenknight <gwen.knight@lshtm.ac.uk>
2023-07-25 11:17:02 +0100

    Added in Time optional analysis plots

--- 4_Figures_data_challenge.R
+++ 4_Figures_data_challenge.R
@@ -1,4 +1,4 @@
-# This script creates the figures used in the report as part of the Data Challenge
+# MICAG: This script creates the figures used in the report as part of the Data Challenge
 
 library(data.table);library(ggplot2);library(cowplot); library(patchwork); library(tidyverse)
 theme_set(theme_bw())
@@ -12,8 +12,11 @@ characteristics <- c("age_group", "key_source") # "age_group"
 ############################# Get data
 output_data <- c()
 output_index <- c()
+
 for(i in characteristics){
   print(i)
+  
+  ###### Output no time 
   output1 <- read.csv(paste0("plots/gender_",i, "output.csv")) %>% mutate(charac = i) %>% rename("charac_value" = i)
   output2 <- read.csv(paste0("plots/",i, "output.csv")) %>% mutate(gender = "N", charac = i) %>% rename("charac_value" = i)
   output_data <- rbind(rbind(output_data, 
@@ -46,16 +49,17 @@ for(i in 1:nrow(combinations)){
   plot_data <- rbind(plot_data,
                      output_data %>% filter(organism == combinations[i,"organism"],
                                             antibiotic == combinations[i,"antibiotic"]))
+  
   plot_index <- rbind(plot_index, 
                       output_index %>% filter(organism == combinations[i,"organism"],
-                                            antibiotic == combinations[i,"antibiotic"])) 
+                                              antibiotic == combinations[i,"antibiotic"])) 
 }
 
 #################################### Age only
 plot_age <- plot_data %>% filter(charac == "age_group")
 plot_age$charac_value <- factor(plot_age$charac_value, 
-                             levels = c("0 to 2 Years","3 to 12 Years", "13 to 18 Years",
-                                        "19 to 64 Years", "65 to 84 Years", "85 and Over"))
+                                levels = c("0 to 2 Years","3 to 12 Years", "13 to 18 Years",
+                                           "19 to 64 Years", "65 to 84 Years", "85 and Over"))
 
 ggplot(plot_age %>% filter(gender == "N"), 
        aes(x=MIC, y = cumulative_sum, group = charac_value)) + 
@@ -68,7 +72,7 @@ ggsave("plots/fig2_age.pdf")
 
 # Age and gender 
 g1 <- ggplot(plot_age %>% filter(!gender == "N"), 
-       aes(x=MIC, y = cumulative_sum, group = interaction(gender,charac_value))) + 
+             aes(x=MIC, y = cumulative_sum, group = interaction(gender,charac_value))) + 
   geom_line(aes(col = charac_value, lty = gender)) + 
   facet_grid(organism ~ antibiotic, scales = "free") + 
   scale_x_log10("MIC", labels = scales::comma) + 
@@ -140,5 +144,93 @@ b <- g1 + g3 + plot_layout(guides = "collect") & theme(legend.position = "bottom
 a / b + plot_layout(heights = c(1, 1.8))
 ggsave("plots/fig2_v2.pdf", width = 20, height = 11)
 
+###### Final figure 
+##### OVER TIME: OPTIONAL
+# read in the data
+############################# Get data
+output_datat <- c()
+
+for(i in characteristics){
+  print(i)
+  
+  ###### Output with time 
+  output1t <- read.csv(paste0("plots/year_gender_",i, "output.csv")) %>% mutate(charac = i) %>% rename("charac_value" = i)
+  output2t <- read.csv(paste0("plots/year_",i, "output.csv")) %>% mutate(gender = "N", charac = i) %>% rename("charac_value" = i)
+  output_datat <- rbind(rbind(output_datat, 
+                              output1t %>% select("year","gender", "MIC", "charac", "charac_value", "N", "Total", "prop", "cumulative_sum", 
+                                                  "antibiotic", "organism")), 
+                        output2t %>% select("year","gender", "MIC", "charac", "charac_value", "N", "Total", "prop", "cumulative_sum", 
+                                            "antibiotic", "organism"))
+}
+
+### EXAMPLES
+combinations <- as.data.frame(rbind(c("Staphylococcus aureus", "levofloxacin"),
+                                    c("Escherichia coli", "levofloxacin"),
+                                    c("Staphylococcus aureus", "meropenem"),
+                                    c("Escherichia coli", "meropenem"),
+                                    c("Staphylococcus aureus", "ampicillin"),
+                                    c("Escherichia coli", "ampicillin")))
+colnames(combinations) <- c("organism","antibiotic")
+
+
+# Grab just the data wanted for the examples
+plot_datat <- c()
+
+for(i in 1:nrow(combinations)){
+  plot_datat <- rbind(plot_datat,
+                      output_datat %>% filter(organism == combinations[i,"organism"],
+                                              antibiotic == combinations[i,"antibiotic"]))
+}
+
+# Index 
+index_comparison_gender_yr <-read_csv(paste0("plots/year_gender_",characteristic,"index_store.csv"))
+
+# Explore data: how many high level and extract max level over time 
+sum_index_gender_yr <- index_comparison_gender_yr %>% 
+  group_by(antibiotic, organism, gender, MIC, year) %>%
+  summarise(df_mic = max(dff)) %>% # Get one value per MIC 
+  group_by(antibiotic, organism, gender, year) %>%
+  summarise(mx = max(df_mic), # Max diff for this bug_drug 
+            n_big = sum(unique(df_mic) > 0.1)) # Count how many MIC have > 10% diffs
+
+# heat map
+gg <- sum_index_gender_yr %>% filter(n_big > 3)
+ggplot(gg, aes(x=year, y = antibiotic, z = mx)) + 
+  geom_tile(aes(fill = mx)) + 
+  facet_grid(gender~organism) + 
+  ggtitle(characteristic) + 
+  scale_fill_continuous("Maximum\nindex") + 
+  theme(strip.text = element_text(face = "italic"))
+ggsave(paste0("plots/", characteristic, "index_time_heat_map_allbac.pdf"), height = 7, width = 15)
+
+ggplot(gg %>% filter(organism %in% c("Staphylococcus aureus","Escherichia coli")), aes(x=year, y = antibiotic, z = mx)) + 
+  geom_tile(aes(fill = mx)) + 
+  facet_grid(gender~organism) + 
+  ggtitle(characteristic) + 
+  scale_fill_continuous("Maximum\nindex") + 
+  theme(strip.text = element_text(face = "italic"))
+ggsave(paste0("plots/", characteristic, "index_time_heat_map.pdf"), height = 7, width = 15)
+
+## Over time 
+plot_datat_staphlevo <- plot_datat %>% filter(antibiotic == "levofloxacin",
+                                              organism == "Staphylococcus aureus", 
+                                              charac == "age_group") %>%
+  ungroup %>% 
+  group_by(gender, MIC, charac_value) %>% mutate(total_iso = sum(Total))
+
+ggplot(plot_datat_staphlevo %>% filter(total_iso > 1000), 
+       aes(x = year, y = cumulative_sum, colour = charac_value, 
+           group = interaction(gender, charac_value))) + 
+  geom_line(aes(linetype = factor(gender))) + 
+  facet_wrap(~MIC) + 
+  scale_x_log10()
+ggsave(paste0("plots/", characteristic, "index_time_heat_map.pdf"), height = 7, width = 15)
+
+ggplot(plot_datat_staphlevo %>% filter(total_iso > 10000), 
+       aes(x = year, y = cumulative_sum, colour = charac_value, 
+           group = interaction(gender, charac_value))) + 
+  geom_line(aes(linetype = factor(gender))) + 
+  facet_wrap(~MIC) + 
+  scale_x_log10()
 
 
